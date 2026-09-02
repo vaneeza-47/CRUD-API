0bf14a4a8781d88043c41133006807f1d91c1392
vaneeza <vaneeza48.chgmail.com>
2026-09-02 23:22:26 +0500

    Stage 0: create SQLite database

---
+++ src/repositories/database.js
@@ -0,0 +1,39 @@
+const sqlite = require("better-sqlite3");
+
+// Initiate Database
+const db = new sqlite.Database('tasks.db');
+
+const createTable = `CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    done INTEGER
+)`;
+
+db.exec(createTable);
+console.log("Tasks Table Ready!");
+
+const count = db.prepare(`SELECT COUNT(*) FROM tasks`).get();
+
+// Dummy tasks if table is empty
+if (count.count === 0) {
+    console.log("Seeding example tasks...");
+    const dummyData = [
+        {title: "Learn better-sqlite3", done: 0},
+        {title: "Eat more ice-cream", done: 0},
+        {title: "Complete assignment 2", done: 0}
+    ];
+    const prepareData = db.prepare(`INSERT INTO tasks (title, done) VALUES (?, ?)`);
+
+    dummyData.forEach((task) => {
+        prepareData.run(task.title, task.done);
+    });
+
+    const selectTasks = "SELECT * FROM tasks";
+    const currentTasks = db.prepare(selectTasks).all();
+    console.log(currentTasks);
+} else {
+    console.log(`Found ${count.count} tasks already. Skipping Seeding`);
+}
+
+module.exports = db;
+
